177e1b2e4734f744d7a3b55b244a356a5e220305
Peixian Wang <peixian@driftingicebergs.com>
2016-02-24 13:29:00 -0600

    Increase the calendar scraper number, and also write it to a txt

--- validCalendars.py
+++ validCalendars.py
@@ -4,9 +4,13 @@ calendarFormat = "https://illinois.edu/calendar/list/{}"
 
 validCalendars = []
 #I bet you've never seen such a hacky way to scrape calendars
-for i in range(0, 5000):
+for i in range(0, 10000):
     r = requests.get(calendarFormat.format(i))
-    if r.status_code is not 302:
+    #if all is well with the calendar
+    if r.status_code is 200:
         validCalendars.append(i)
         
-print(validCalendars)
+#print(validCalendars)
+with open("validCalendars.txt", "w") as file:
+    for validCalendarCode in validCalendars:
+        file.write(calendarFormat.format(validCalendarCode))
